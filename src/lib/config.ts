800fb7b246def42a5267ae97337cd837165a6212
Sercan Isincelik <sercan_isincelik@hotmail.com>
2024-04-15 08:55:47 +0200

    init. commit

---
+++ src/lib/config.ts
@@ -0,0 +1,12 @@
+export const SHOP_URL = 'https://shop.example.com';
+
+export const INSTAGRAM_URL = 'https://www.instagram.com/managamesberlin/';
+export const YOUTUBE_URL = 'https://www.youtube.com/channel/example';
+export const TWITCH_URL = 'https://www.twitch.tv/managames2023';
+export const TWITTER_URL = 'https://www.twitter.com/example';
+
+export const TOURNAMENTS = [
+  "Testturnier 3, 17.07.2021 um 18:00 Uhr",
+  "Testturnier 2, 21.07.2021 um 12:00 Uhr",
+  "Testturnier 1, 24.07.2021 um 15:00 Uhr",
+];
