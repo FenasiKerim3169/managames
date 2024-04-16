1ce004ab0a547f71332ffd7f9c5578881b8c9611
Sercan Isincelik <sercan_isincelik@hotmail.com>
2024-04-16 13:12:56 +0200

    seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), sitemap()],
   server: {
     port: 8080
-  }
+  },
+  site: 'https://managames.de',
 });
